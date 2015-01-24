6879a8d9214ce87b3e5b3841b6d9bda4222483f6
Antonio Fabregat <fabregat.antonio@gmail.com>
2015-01-24 10:48:13 +0000

    minor change

--- Widget/src/main/java/org/reactome/web/fireworks/client/FireworksViewer.java
+++ Widget/src/main/java/org/reactome/web/fireworks/client/FireworksViewer.java
@@ -10,6 +10,7 @@ import org.reactome.web.fireworks.model.Node;
 /**
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  */
+@SuppressWarnings("UnusedDeclaration")
 public interface FireworksViewer extends IsWidget, HasHandlers, RequiresResize {
 
     public HandlerRegistration addAnalysisResetHandler(AnalysisResetHandler handler);
