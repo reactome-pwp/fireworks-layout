b193a667f6d2151969659b19e4d72b91f3450793
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2016-04-21 14:18:31 +0100

    Changed to flowpanel

--- Widget/src/main/java/org/reactome/web/fireworks/search/fallback/panels/AbstractAccordionPanel.java
+++ Widget/src/main/java/org/reactome/web/fireworks/search/fallback/panels/AbstractAccordionPanel.java
@@ -1,7 +1,7 @@
 package org.reactome.web.fireworks.search.fallback.panels;
 
 import com.google.gwt.dom.client.Style;
-import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.FlowPanel;
 import org.reactome.web.fireworks.search.fallback.events.PanelCollapsedEvent;
 import org.reactome.web.fireworks.search.fallback.events.PanelExpandedEvent;
 
@@ -9,7 +9,7 @@ import org.reactome.web.fireworks.search.fallback.events.PanelExpandedEvent;
 /**
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  */
-public class AbstractAccordionPanel extends SimplePanel implements SearchAccordionPanel {
+public class AbstractAccordionPanel extends FlowPanel implements SearchAccordionPanel {
 
     @Override
     public void onPanelCollapsed(PanelCollapsedEvent event) {
